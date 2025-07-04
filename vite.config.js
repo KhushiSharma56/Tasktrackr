2f4acf0783d51e67ce68332eaadab68f89f51433
KhushiSharma56 <165081515+KhushiSharma56@users.noreply.github.com>
2025-07-04 20:24:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Tasktrackr/", // 👈 your GitHub repo name (case-sensitive)
+  base: "/Tasktrackr/", 
 })
